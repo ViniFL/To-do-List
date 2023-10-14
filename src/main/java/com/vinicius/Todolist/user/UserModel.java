3c503d98b2331c8c0fca688f4190e2eb2993a8ba
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-10-13 23:43:00 -0300

    User, Lombok, H2

---
+++ src/main/java/com/vinicius/Todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package com.vinicius.Todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String username;
+    private String name;
+    private String password;
+
+}
