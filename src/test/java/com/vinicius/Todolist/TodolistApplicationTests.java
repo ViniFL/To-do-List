61308896ba240ac86ddf889bc5aa8248eaa1a938
Vinicius Fernandes <vinicius.fernandes.lima@outlook.com>
2023-10-13 22:34:24 -0300

    fist commit

---
+++ src/test/java/com/vinicius/Todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.Todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
